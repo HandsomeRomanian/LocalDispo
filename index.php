bbda059ee8dd6651b96534194d28fc8d3610cccb
Matei Martin <mat4school@gmail.com>
2019-04-07 22:38:19 -0400

    Removing useless dependencies.

--- index.php
+++ index.php
@@ -6,15 +6,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
 	<!-- Bootstrap core CSS -->
-	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="node_modules\@fortawesome\fontawesome-free\css\all.min.css" rel="stylesheet" type="text/css">
+
 
 	<!-- Custom fonts for this template -->
-	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
 	<!-- Plugin CSS -->
-	<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
 
 	<!-- Custom styles for this template -->
 	<link href="css/freelancer.min.css" rel="stylesheet">
@@ -54,10 +55,8 @@
 
 	<section>
 		<header class="masthead">
-			<content>
-				<h1 class="text-center text-uppercase ">Tire</h1>
-				<h2 class="text-center font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
-			</content>
+				<h1 class="text-center text-uppercase ">D-6969</h1>
+				<h2 class="text-center font-weight-light mb-0"></h2>
 		</header>
 
 
@@ -94,8 +93,8 @@
 						<div class="top-info"><span>Monday</span></div>
 
 						<ul>
-							<li class="single-event" data-start="08:40" data-end="9:40" data-content="reseau1" data-event="class-1">
-								<a href="#0">
+							<li href="/test.php" class="single-event" data-start="08:40" data-end="9:40" data-content="reseau1" data-event="class-1">
+								<a href="test">
 									<em class="class-name">Reseau1</em>
 								</a>
 							</li>
@@ -112,7 +111,7 @@
 								</a>
 							</li>
 
-							<li class="single-event" data-start="13:10" data-end="15:10" data-content="BD2" data-event="class-3">
+							<li class="single-event" data-start="13:10" data-end="15:10" data-content="prog1" data-event="class-3">
 								<a href="#0">
 									<em class="class-name">Programation O</em>
 								</a>
@@ -124,31 +123,31 @@
 						<div class="top-info"><span>Tuesday</span></div>
 
 						<ul>
-							<li class="single-event" data-start="9:00" data-end="10:00" data-content="rowing-workout" data-event="class-2">
+							<li class="single-event" data-start="9:00" data-end="10:00" data-content="reseau1" data-event="class-2">
 								<a href="#0">
 									<em class="class-name">Réseau 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="10:40" data-end="12:10" data-content="restorative-yoga" data-event="class-4">
+							<li class="single-event" data-start="10:40" data-end="12:10" data-content="secu" data-event="class-4">
 								<a href="#0">
 									<em class="class-name">Surv et sécu. 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="12:40" data-end="14:10" data-content="abs-circuit" data-event="class-1">
+							<li class="single-event" data-start="12:40" data-end="14:10" data-content="reseau1" data-event="class-1">
 								<a href="#0">
 									<em class="class-name">Réseau 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="15:40" data-end="16:40" data-content="yoga-1" data-event="class-3">
+							<li class="single-event" data-start="15:40" data-end="16:40" data-content="prog1" data-event="class-3">
 								<a href="#0">
 									<em class="class-name">Programation O</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="15:40" data-end="16:40" data-content="yoga-1" data-event="class-3">
+							<li class="single-event" data-start="15:40" data-end="16:40" data-content="prog1" data-event="class-3">
 								<a href="#0">
 									<em class="class-name">Programation O</em>
 								</a>
@@ -160,25 +159,25 @@
 						<div class="top-info"><span>Wednesday</span></div>
 
 						<ul>
-							<li class="single-event" data-start="08:10" data-end="10:10" data-content="restorative-yoga" data-event="class-4">
+							<li class="single-event" data-start="08:10" data-end="10:10" data-content="secu" data-event="class-4">
 								<a href="#0">
 									<em class="class-name">Surv et sécu. 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="10:10" data-end="13:40" data-content="rowing-workout" data-event="class-2">
+							<li class="single-event" data-start="10:10" data-end="13:40" data-content="reseau1" data-event="class-2">
 								<a href="#0">
 									<em class="class-name">Réseau 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="13:40" data-end="15:10" data-content="yoga-1" data-event="class-3">
+							<li class="single-event" data-start="13:40" data-end="15:10" data-content="prog1" data-event="class-3">
 								<a href="#0">
 									<em class="class-name">Programation O</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="15:10" data-end="17:10" data-content="yoga-1" data-event="class-3">
+							<li class="single-event" data-start="15:10" data-end="17:10" data-content="prog1" data-event="class-3">
 								<a href="#0">
 									<em class="class-name">Programation O</em>
 								</a>
@@ -190,25 +189,25 @@
 						<div class="top-info"><span>Thursday</span></div>
 
 						<ul>
-							<li class="single-event" data-start="08:40" data-end="9:40" data-content="abs-circuit" data-event="class-1">
+							<li class="single-event" data-start="08:40" data-end="9:40" data-content="reseau1" data-event="class-1">
 								<a href="#0">
 									<em class="class-name">Réseau 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="10:10" data-end="13:40" data-content="restorative-yoga" data-event="class-4">
+							<li class="single-event" data-start="10:10" data-end="13:40" data-content="secu" data-event="class-4">
 								<a href="#0">
 									<em class="class-name">Surv et sécu. 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="14:40" data-end="15:40" data-content="abs-circuit" data-event="class-1">
+							<li class="single-event" data-start="14:40" data-end="15:40" data-content="reseau1" data-event="class-1">
 								<a href="#0">
 									<em class="class-name">Réseau 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="16:10" data-end="18:00" data-content="rowing-workout" data-event="class-2">
+							<li class="single-event" data-start="16:10" data-end="18:00" data-content="reseau1" data-event="class-2">
 								<a href="#0">
 									<em class="class-name">Réseau 1</em>
 								</a>
@@ -220,25 +219,25 @@
 						<div class="top-info"><span>Friday</span></div>
 
 						<ul>
-							<li class="single-event" data-start="9:00" data-end="10:00" data-content="rowing-workout" data-event="class-2">
+							<li class="single-event" data-start="9:00" data-end="10:00" data-content="reseau1" data-event="class-2">
 								<a href="#0">
 									<em class="class-name">Réseau 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="11:40" data-end="13:10" data-content="abs-circuit" data-event="class-1">
+							<li class="single-event" data-start="11:40" data-end="13:10" data-content="reseau1" data-event="class-1">
 								<a href="#0">
 									<em class="class-name">Réseau 1</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="15:40" data-end="16:40" data-content="yoga-1" data-event="class-3">
+							<li class="single-event" data-start="15:40" data-end="16:40" data-content="prog1" data-event="class-3">
 								<a href="#0">
 									<em class="class-name">Programation O</em>
 								</a>
 							</li>
 
-							<li class="single-event" data-start="15:40" data-end="16:40" data-content="yoga-1" data-event="class-3">
+							<li class="single-event" data-start="15:40" data-end="16:40" data-content="prog1" data-event="class-3">
 								<a href="#0">
 									<em class="class-name">Programation O</em>
 								</a>
@@ -249,21 +248,21 @@
 			</div>
 
 			<div class="class-modal">
-				<header class="header">
+				<div class="header">
 					<div class="content">
 						<span class="class-date"></span>
 						<h3 class="class-name"></h3>
 					</div>
 
 					<div class="header-bg"></div>
-				</header>
+				</div>
 
 				<div class="body">
-					<div class="class-info">ErrorL: content not found.</div>
+					<div class="class-info">Error: content not found.</div>
 					<div class="body-bg"></div>
 				</div>
 
-				<a href="#0" class="close">Close</a>
+				<a href="#0" class="close"></a>
 			</div>
 
 			<div class="cover-layer"></div>
@@ -277,8 +276,8 @@
 			  <div class="row">
 				<div class="col-md-4 mb-5 mb-lg-0">
 				  <h4 class="text-uppercase mb-4">Location</h4>
-				  <p class="lead mb-0">2215 John Daniel Drive
-					<br>Clark, MO 65243</p>
+				  <p class="lead mb-0">Montreal
+					<br>Quebec, Canada</p>
 				</div>
 				<div class="col-md-4 mb-5 mb-lg-0">
 				  <h4 class="text-uppercase mb-4">Around the Web</h4>
@@ -312,7 +311,7 @@
 				</div>
 				<div class="col-md-4">
 				  <h4 class="text-uppercase mb-4">Open Classroom Schedule</h4>
-				  <p class="lead mb-0">WIP un completed design.</p>
+				  <p class="lead mb-0">WIP uncompleted design.</p>
 				</div>
 			  </div>
 			</div>
@@ -333,12 +332,14 @@
 
 
 
-	<script src="js/modernizr.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 	<script>
 		if (!window.jQuery) document.write('<script src="js/jquery-3.0.0.min.js"><\/script>');
 	</script>
 	<script src="js/main.js"></script> <!-- Resource jQuery -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
