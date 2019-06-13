02b560220c61cb2c5e98d31de0ae9b5d1d1bbfe7
HandsomeRomanian <mat4school@gmail.com>
2019-06-12 20:34:08 -0400

    Started Work on JSON generator.

--- test.php
+++ test.php
@@ -1,7 +1,29 @@
 <?php
 
 include("controller/functions.php");
-
-echo "18:10" - 7;
+$json_output = json_decode(file_get_contents("json/local.json"));
  
+echo '{"Locals" : [';
+    foreach ($json_output->Locals as $tmp) {
+        echo '{ <br>"Emplacement" : '.$tmp->Emplacement.',<br>"Jours" : [';
+            for ($i=0; $i < 5; $i++) { 
+                echo '{ <br>"Cours" : <br>[';
+                    foreach ($tmp->Jours[$i]->Cours as $key) {
+                        echo '{ "Nom" : "'.$key->Nom.'",
+                            "Prof" : "'.$key->Prof.'",<br>
+                            "Start" : "'.$key->Start.'",<br>
+                            "End" : "'.$key->End.'",<br>
+                            "ID" : "'.$key->ID.'"<br>
+                            },';
+                    }
+                    if($i < 4){
+                        echo ",";
+                    }
+                    else{
+                        echo "]";
+        
+                    }
+            }
+                
+    }
 ?>
