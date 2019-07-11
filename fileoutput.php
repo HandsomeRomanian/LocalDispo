d447df953a243bec108b9635521707099b1f616b
HandsomeRomanian <mat4school@gmail.com>
2019-07-10 21:55:32 -0400

    created individual script for json creator. [WIP}

---
+++ fileoutput.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("controller/functions.php");
+$json_output = json_decode(file_get_contents("json/local.json"));
+ 
+echo '{
+    "Locals": [';
+    foreach ($json_output->Locals as $tmp) {
+        echo '{ <br>"Emplacement" : '.$tmp->Emplacement.',<br>"Jours" : [';
+            for ($i=0; $i < 5; $i++) { 
+                echo '{ <br>"Cours" : <br>[';
+                    foreach ($tmp->Jours[$i]->Cours as $key) {
+                        echo '{ "Nom" : "'.$key->Nom.'",
+                            "Prof" : "'.$key->Prof.'",<br>
+                            "Start" : "'.$key->Start.'",<br>
+                            "End" : "'.$key->End.'",<br>
+                            "ID" : "'.$key->ID.'"<br>
+                            },';
+                    }
+                    if($i < 4){
+                        echo ",";
+                    }
+                    else{
+                        echo "]";
+        
+                    }
+            }
+                
+    }
+?>
