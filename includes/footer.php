a5ebf96f5487bf78c2b1c87252d776f12ee50a7d
HandsomeRomanian <mat4school@gmail.com>
2019-09-05 03:12:33 +0000

    [CSS] Fixed fontawsome and removed broken back to top button

--- includes/footer.php
+++ includes/footer.php
@@ -9,10 +9,7 @@
 				</div>
 				<div class="col-md-4 mb-5 mb-lg-0 mediaIcons">
 					<h4 class="text-uppercase mb-4">Around the Web</h4>
-          <a href="#" class="fab fa-facebook"></a>
-          <i class="fab fa-facebook"></i>
-					<a href="#" class="fab fa-twitter"></a>
-					<a href="#" class="fab fa-github"></a>
+					<a href="https://github.com/HandsomeRomanian" class="fab fa-github"></a>
 
 				</div>
 				<div class="col-md-4">
@@ -29,15 +26,6 @@
 		</div>
 	</footer>
 
-
-
-<!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
-<div class="scroll-to-top d-lg-none position-fixed ">
-	<a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
-		<i class="fa fa-chevron-up"></i>
-	</a>
-</div>
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
 	integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
@@ -45,3 +33,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
 	integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
 </script>
+<script src="https://kit.fontawesome.com/edbbb5c884.js"></script>
