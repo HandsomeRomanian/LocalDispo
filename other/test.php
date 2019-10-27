b1e34eba65c5b0eb90f1be0716230d811c5b0c40
HandsomeRomanian <mat4school@gmail.com>
2019-10-27 03:07:49 +0000

    get works

--- other/test.php
+++ other/test.php
@@ -1,15 +1,14 @@
 <?php
 
 $response =  json_decode(file_get_contents('http://192.168.0.190:8080/classes/10'),true);
-echo file_get_contents('http://192.168.0.190:8080/classes/10');
-echo $_GET['local'];
-echo $response;
-foreach ($response->classID as $tmp) {
-    echo 'allo';
-    echo $tmp;
-    echo $tmp->classID;
+echo file_get_contents('http://192.168.0.190:8080/classes/10').'<br>';
+echo $_GET['local'].'<br>';
+echo $response->{'0'}->classID;
+Classe classe = new Classe(); 
+foreach ($response as $tmp) {
+    echo $tmp['classID'];
     if ($tmp->classID == $_GET['local']){
-        echo 'GOOD '.$tmp->classID;
+        echo 'GOOD '.$tmp['classID'];
     }
 
 }
