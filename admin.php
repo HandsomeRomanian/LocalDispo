0e1ddcfb8b1e96ab39c6d35290f8a555062c1296
HandsomeRomanian <mat4school@gmail.com>
2019-10-28 15:04:30 +0000

    class add form finished

--- admin.php
+++ admin.php
@@ -20,27 +20,42 @@ $profs = null;
 <head>
     <link rel="stylesheet" href="/css/admin.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
-    <script src="js/moment.js"></script>
-    <script>
-        $('#time').bootstrapMaterialDatePicker({
-            date: false
-        });
-    </script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet">
 
 </head>
 
 <body>
-    <div class="container">
+    <div class="thing">
         <form id="contact" action="" method="post">
-            <h3><?php echo $profsJSON[0]['teacherFirstName'];  ?></h3>
-            <h4>Contact us for custom quote</h4>
-            <input placeholder="Heure de début" name="" type="time" tabindex="1" required autofocus>
-            <br>
-            <input placeholder="Heure de fin" name="" type="time" tabindex="2" required autofocus>
-            <br>
-            <select name="dayID">
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="form-group">
+                        <div class="input-group timepicker">
+                            <input type="text" class="form-control" default="08:00" readonly>
+                            <span class="input-group-addon">
+                                <span class="fa fa-clock-o"></span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="form-group">
+                        <div class="input-group timepicker">
+                            <input type="text" class="form-control" readonly>
+                            <span class="input-group-addon">
+                                <span class="fa fa-clock-o"></span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <select class="form-control" name="dayID" tabindex="3">
                 <option value="1">Lundi</option>
                 <option value="2">Mardi</option>
                 <option value="3">Mercredi</option>
@@ -48,40 +63,52 @@ $profs = null;
                 <option value="5">Vendredi</option>
             </select>
             <br>
-            <select name="localID">
+            <select class="form-control" name="localID" tabindex="4">
                 <?php
-                    foreach ($roomsJSON as $room) {
+                foreach ($roomsJSON as $room) {
+                    $room = new Room($room['localID'], $room['wing'], $room['floor'], $room['number'], $room['places'], $room['typeID']);
+                    ?>
+                    <option value="<?php echo $room->localID; ?>"><?php echo $room->getFull(); ?></option>
+                <?php
+                }
                 ?>
-                <option value="<?php echo $room['localID'];?>"><?php echo $room['teacherFirstName'];?></option>
-                <?php
-            }
-            ?>
             </select>
             <br>
-            <select name="teacherID">
+            <select class="form-control" name="teacherID" tabindex="5">
                 <?php
-                    foreach ($profsJSON as $prof) {
+                foreach ($profsJSON as $prof) {
+                    ?>
+                    <option value="<?php echo $prof['teacherID']; ?>"><?php echo $prof['teacherFirstName'] . " " . $prof['teacherFamName']; ?></option>
+                <?php
+                }
                 ?>
-                <option value="<?php echo $prof['teacherID'];?>"><?php echo $prof['teacherFirstName']." ".$prof['teacherFamName'];?></option>
-                <?php
-            }
-            ?>
             </select>
             <br>
-            <select name="coursID">
+            <select class="form-control" name="coursID" tabindex="6">
                 <?php
-                    foreach ($classesJSON as $class) {
+                foreach ($classesJSON as $class) {
+                    ?>
+                    <option value="<?php echo $class['coursID']; ?>"><?php echo $class['name']; ?></option>
+                <?php
+                }
                 ?>
-                <option value="<?php echo $class['coursID'];?>"><?php echo $class['name'];?></option>
-                <?php
-            }
-            ?>
             </select>
-            <input placeholder="Numero du groupe" name="groupNumber" type="number" tabindex="6" required autofocus>
+            <br>
+            <input class="form-control" min="1000" max="1090" placeholder="Numero du groupe" name="groupNumber" type="number" tabindex="7" required autofocus>
             <br>
             <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-        </form>
     </div>
+    </form>
+    </div>
+
+
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/locale/nl.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+    <script type="text/javascript" src="/js/admin.js"></script>
 </body>
 
 </html>
